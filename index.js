8b1123143b0f132a0fea66ee4b0d9d816c0762e5
root <root@DESKTOP-6I3RBRC>
2023-06-09 20:06:11 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//let spy;
+const newArray = []
+function writeCards(names, surprise){
+  
+    for (let i = 0; i < names.length ; i++)
+    { 
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+    
+    }
+   return newArray
+}
+
+
+function countDown(number) {
+    while (number>=0){
+        console.log(number--);
+    }
+
+
+}
